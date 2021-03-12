61494f1d737f63d4e237735cd4868c0d24606176
Nosferatu721 <elkintorres721@gmail.com>
2021-03-11 21:06:11 -0500

    Diseño Basico terminado

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -8,20 +8,24 @@ class Badge extends React.Component {
   render() {
     return (
       <div className="Badge">
-        <div className="">
+        <div className="Badge__header">
           <img src={logo} alt="Logo Conf" />
         </div>
-        <div className="">
-          <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+        <div className="Badge__section-name">
+          <img
+            className="Badge__avatar"
+            src="https://s.gravatar.com/avatar/1a7c5a12e5a46cca9ad5f5d20968ddc0?s=80"
+            alt="Avatar"
+          />
           <h1>
             Elkin <br /> Torres
           </h1>
         </div>
-        <div className="">
-          <p>Frontend Enginer :3</p>
-          <p>@Nosferatu</p>
+        <div className="Badge__section-info">
+          <h3>Frontend Enginer :3</h3>
+          <div>@Nosferatu</div>
         </div>
-        <div className="">
+        <div className="Badge__footer">
           <p>#PlatziConf</p>
         </div>
       </div>
